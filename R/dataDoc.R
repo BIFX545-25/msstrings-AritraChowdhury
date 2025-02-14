fb5f2c491ddf0323c898b34bed2f2f6f08065ac1
AritraChowdhury <aritra15503@gmail.com>
2025-02-13 19:58:48 -0500

    Added data and tests

---
+++ R/dataDoc.R
@@ -0,0 +1,10 @@
+#' MSfrags
+#' 
+#' This is the sample data set that will be provided with this package, it is some protein
+#' fragments with modifications we may expect from the CCR5 gene in the mass spec experiment
+#'  
+#' 
+#' @format A character vector of length 4 with peptide sequences
+#' 
+#' @source data-raw/data.R
+"MSfrags"
